b7755fe0ae6cb78e16408e02674ce8c229f5d6b9
yihui-he <yihuihe@foxmail.com>
2018-11-03 15:27:49 -0400

    toy

--- Toy_Regression.py
+++ Toy_Regression.py
@@ -1,165 +1,68 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-from caffe2.python import workspace, brew, optimizer
-from caffe2.python.model_helper import ModelHelper
+from caffe2.python import core, cnn, net_drawer, workspace, visualize
 import numpy as np
-import matplotlib.pyplot as plt
-import sklearn.datasets
-from sklearn.preprocessing import PolynomialFeatures
+from IPython import display
+from matplotlib import pyplot
 
-# Number of training sample to generate
-num_samples = 200
-# Learning Rate of SGD algorithm
-learning_rate = .05
-# Number of iterations to train
-training_iters = 100
-# Initial model weights
-initial_weights = [0.,0.]
+init_net = core.Net("init")
+# The ground truth parameters.
+W_gt = init_net.GivenTensorFill(
+            [], "W_gt", shape=[1, 2], values=[2.0, 1.5])
+B_gt = init_net.GivenTensorFill([], "B_gt", shape=[1], values=[0.5])
+# Constant value ONE is used in weighted sum when updating parameters.
+ONE = init_net.ConstantFill([], "ONE", shape=[1], value=1.)
+# ITER is the iterator count.
+ITER = init_net.ConstantFill([], "ITER", shape=[1], value=0, dtype=core.DataType.INT32)
 
-# Create the original observations
-orig_X,_ = sklearn.datasets.make_regression(n_samples=num_samples,n_features=1,noise=5)
-poly = PolynomialFeatures(degree=2, include_bias=False)
-# Transform the features into second order polynomial features
-xx_ = poly.fit_transform(orig_X)
+# For the parameters to be learned: we randomly initialize weight
+# from [-1, 1] and init bias with 0.0.
+W = init_net.UniformFill([], "W", shape=[1, 2], min=-1., max=1.)
+B = init_net.ConstantFill([], "B", shape=[1], value=0.0)
+print('Created init net.')
 
-# Extract the predictors and the values from the manufactured data
-X = [i[0] for i in xx_]
-Y_gt = [i[1] for i in xx_]
-noise = np.random.uniform(size=(len(Y_gt)))
-# Add some noise to the ground truth values
-Y_gt += noise
+train_net = core.Net("train")
+# First, we generate random samples of X and create the ground truth.
+X = train_net.GaussianFill([], "X", shape=[64, 2], mean=0.0, std=1.0, run_once=0)
+Y_gt = X.FC([W_gt, B_gt], "Y_gt")
+# We add Gaussian noise to the ground truth
+noise = train_net.GaussianFill([], "noise", shape=[64, 1], mean=0.0, std=1.0, run_once=0)
+Y_noise = Y_gt.Add(noise, "Y_noise")
+# Note that we do not need to propagate the gradients back through Y_noise,
+# so we mark StopGradient to notify the auto differentiating algorithm
+# to ignore this path.
+Y_noise = Y_noise.StopGradient([], "Y_noise")
 
-# Shape the ground truth values for use with the model
-Y_gt = np.reshape(Y_gt,(-1,1))
-# Format the input features. Recall, we accomplish polynomial regression by
-#   including the original and the polynomial version of the predictors
-#   as features of the model
-X = np.hstack((np.array(X).reshape(-1,1),np.array(X).reshape(-1,1)**2))
+# Now, for the normal linear regression prediction, this is all we need.
+Y_pred = X.FC([W, B], "Y_pred")
 
-# Print a sample of the input data. X is the list of 2-feature input observations 
-#   and Y is the list of ground truth values associated with each observation
-print("X Sample:\n{}".format(X[:5]))
-print("Y Sample:\n{}".format(Y_gt[:5]))
+# The loss function is computed by a squared L2 distance, and then averaged
+# over all items in the minibatch.
+dist = train_net.SquaredL2Distance([Y_noise, Y_pred], "dist")
+loss = dist.AveragedLoss([], ["loss"])
 
-# Plot the input data
-# plt.scatter([i[0] for i in X],Y_gt,label="original data",color='b')
-# plt.xlabel("x")
-# plt.ylabel("y")
-# plt.title("Input Training Data")
+# Increment the iteration by one.
+train_net.Iter(ITER, ITER)
+# Compute the learning rate that corresponds to the iteration.
+LR = train_net.LearningRate(ITER, "LR", base_lr=-0.1,
+                                    policy="step", stepsize=20, gamma=0.9)
 
-# Create the model helper object we will use to create the regression model
-regression_model = ModelHelper(name="regression_model")
+gradient_map = train_net.AddGradientOperators([loss])
+# Weighted sum
+train_net.WeightedSum([W, ONE, gradient_map[W], LR], W)
+train_net.WeightedSum([B, ONE, gradient_map[B], LR], B)
 
-# Add the FC layer, which is the main component of a linear regression model
-y_pred = brew.fc(regression_model,'X','y_pred', dim_in=2, dim_out=1)
+workspace.RunNetOnce(init_net)
+workspace.CreateNet(train_net)
 
-# Print the predict and init net to see what protobuf was created for this model
-print("************* Predict Net *************")
-print(regression_model.net.Proto())
-print("\n************* Init Net *************")
-print(regression_model.param_init_net.Proto())
+print("Before training, W is: {}".format(workspace.FetchBlob("W")))
+print("Before training, B is: {}".format(workspace.FetchBlob("B")))
 
-# The loss function is computed by a squared L2 distance, 
-#   and then averaged over all items.
-dist = regression_model.SquaredL2Distance(['Y_gt', y_pred], "dist")
-loss = regression_model.AveragedLoss(dist, "loss")
-
-# Add the gradient operators and setup the SGD algorithm
-regression_model.AddGradientOperators([loss])
-optimizer.build_sgd(regression_model, base_learning_rate=learning_rate)
-
-# Prime the workspace with some data
-workspace.FeedBlob("Y_gt",Y_gt.astype(np.float32))
-workspace.FeedBlob("X",X.astype(np.float32))
-
-# Run the init net to prepare the workspace then create the net
-workspace.RunNetOnce(regression_model.param_init_net)
-workspace.CreateNet(regression_model.net)
-
-# Inject our desired initial weights and bias
-workspace.FeedBlob("y_pred_w",np.array([initial_weights]).astype(np.float32))
-workspace.FeedBlob("y_pred_b",np.array([0.]).astype(np.float32))
-
-
-# Run the training for training_iters
-for i in range(training_iters):
-    workspace.RunNet(regression_model.net)
-
-print("Training Complete")
-
-
-# Extract the learned coes and intercept from the workspace
-coes = workspace.FetchBlob("y_pred_w")[0]
-intercept = workspace.FetchBlob("y_pred_b")
-
-# Calculate the regression line for plotting
-x_vals = np.linspace(orig_X.min(), orig_X.max(),100)
-regression_result = intercept[0] + coes[0]*x_vals + coes[1]*(x_vals**2)
-print("Best Fit Line: {}*x^2 + {}*x + {}".format(round(coes[1],5), round(coes[0],5), round(intercept[0],5)))
-
-# Plot the results of the regression
-plt.scatter([i[0] for i in X],Y_gt,label="original data",color='b')
-plt.plot(x_vals,regression_result,label="regression result",color='r')
-plt.legend()
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("Polynomial Regression Fit: ${{{}}}x^2 + {{{}}}x + {{{}}}$".format(round(coes[1],5), round(coes[0],5), round(intercept[0],5)))
-plt.show()
-
-#####################################################################
-# Initialize data
-#####################################################################
-X,Y_gt = sklearn.datasets.make_regression(n_samples=100,n_features=1,noise=10)
-Y_gt = np.reshape(Y_gt,(-1,1))
-Y_gt /= 100.
-
-#####################################################################
-# Create and train model
-#####################################################################
-# Construct model with single FC layer
-regression_model = ModelHelper(name="regression_model")
-y_pred = brew.fc(regression_model,'X','y_pred', dim_in=1, dim_out=1)
-
-# Specify Loss function
-dist = regression_model.SquaredL2Distance(['Y_gt', y_pred], "dist")
-loss = regression_model.AveragedLoss(dist, "loss")
-
-# Get gradients for all the computations above.
-regression_model.AddGradientOperators([loss])
-optimizer.build_sgd(regression_model, base_learning_rate=0.05)
-
-# Prime and prepare workspace for training
-workspace.FeedBlob("Y_gt",Y_gt.astype(np.float32))
-workspace.FeedBlob("X",X.astype(np.float32))
-workspace.RunNetOnce(regression_model.param_init_net)
-workspace.CreateNet(regression_model.net)
-
-# Set the initial weight and bias to 0
-workspace.FeedBlob("y_pred_w",np.array([[0.]]).astype(np.float32))
-workspace.FeedBlob("y_pred_b",np.array([0.]).astype(np.float32))
-
-# Train the model
 for i in range(100):
-    workspace.RunNet(regression_model.net)
+    workspace.RunNet(train_net.Proto().name)
 
-#####################################################################
-# Collect and format results
-#####################################################################
-# Grab the learned weight and bias from workspace
-coe = workspace.FetchBlob("y_pred_w")[0]
-intercept = workspace.FetchBlob("y_pred_b")
+print("After training, W is: {}".format(workspace.FetchBlob("W")))
+print("After training, B is: {}".format(workspace.FetchBlob("B")))
 
-# Calculate the regression line for plotting
-x_vals = range(-3,4)
-regression_result = x_vals*coe + intercept
+print("Ground truth W is: {}".format(workspace.FetchBlob("W_gt")))
+print("Ground truth B is: {}".format(workspace.FetchBlob("B_gt")))
 
-# Plot the results
-plt.scatter(X,Y_gt,label="original data",color='b')
-plt.plot(x_vals,regression_result,label="regression result",color='r')
-plt.legend()
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("Regression Line: ${{{}}}x + {{{}}}$".format(round(coe,5), round(intercept[0],5)))
-plt.show()
+
